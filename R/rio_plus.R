43e3bbe8f934194701d5b25b36116518558c7aa4
mikebesso <mike.besso@gmail.com>
2017-12-27 10:43:00 -0600

    refactoring

--- R/rio_plus.R
+++ R/rio_plus.R
@@ -8,7 +8,37 @@ ExportDataFramesToExcel <- function(){
 
 
 #' @export
-ImportDataFramesFromExcel <- function(path, whichSheets, rbind = FALSE, rbindLabel = "_file", rbindFill = TRUE){
+ImportDataFrameFromExcelWorkbook <- function(path, whichSheet){
+
+  AssertFileExists(path)
+
+  DataFrames <- rio::import(
+    file = path,
+    setclass = "tibble",
+    which = whichSheet
+  )
+
+  return(DataFrames)
+
+}
+
+#' @export
+ImportDataFramesFromExcelWorkbook <- function(path, whichSheets){
+
+  AssertFileExists(path)
+
+  DataFrames <- rio::import_list(
+    file = path,
+    setclass = "tibble",
+    which = whichSheets
+  )
+
+  return(DataFrames)
+
+}
+
+#' @export
+ImportDataFramesFromExcelWorkbooks <- function(path, whichSheets, rbind = FALSE, rbindLabel = "_file", rbindFill = TRUE){
 
   AssertFileExists(path)
 
